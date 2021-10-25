b33edff8e0de3d2e9733e0cbb3092c9be430d97a
Eric Girouard <gwd@johnabbott.qc.ca>
2021-10-25 16:07:16 -0400

    Added comment functionality
    Added generic comments.php and comments functionality to High Res php template.

--- highres.php
+++ highres.php
@@ -1,15 +1,15 @@
 <?php
-// Add this to all your php files for added security
-
-if (!defined('ABSPATH'))
-	exit; // Exit if accessed directly.
-
-?>
-
-<?php 
 /* Template Name: Highres Template */ 
 /* The template for displaying single posts and pages. */
 /* @link https://developer.wordpress.org/themes/basics/template-hierarchy/  */
+
+
+
+
+// Add this to all your php files for added security
+if (!defined('ABSPATH'))
+	exit; // Exit if accessed directly.
+
 ?>
 
 
@@ -30,6 +30,39 @@ if (!defined('ABSPATH'))
       ?>
       
         <?php if (is_page_template('highres.php')){ eg_CAnav(false); } ?>
+
+
+    <?php // SITE READER COMMENTS  ?>
+
+       <?php //Get only the approved comments
+            $args = array(
+                'status' => 'approve'
+            );
+            
+            // The comment Query
+            $comments_query = new WP_Comment_Query;
+            $comments = $comments_query->query( $args );
+            
+            // Comment Loop
+            if ( $comments ) {
+            foreach ( $comments as $comment ) {
+
+          //  Enable this comment to see the names of the options you can select
+          //  echo '<pre>' . print_r($comment,1) . '</pre>' ;  
+
+
+
+            echo '<div class="commentinfo">' . '<p>' . $comment->comment_author . '</p>';
+            echo '<p>' . $comment->comment_content .'</p>'. '</div>';
+            }
+            } else {
+            echo '<p>' .'No comments found.' . '</p>';
+            }
+
+        ?>
+
+        <?php // ADD COMMENT FORM
+        comment_form(); ?>
 
 </main>
 
