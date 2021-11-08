8707f153b4abb78b800bac8b1c0bb0b516b7650a
Eric Girouard <gwd@johnabbott.qc.ca>
2021-11-08 14:20:50 -0500

    Added responsive features
    Added viewport meta tag, and multiple media query templates for regular and retina displays as well as portrait and landscape orientation.

--- header-thumbs.php
+++ header-thumbs.php
@@ -10,6 +10,7 @@ if (!defined('ABSPATH'))
 <html <?php language_attributes(); ?>>
 <head>
 <meta charset="<?php bloginfo( 'charset' ); ?>" />
+<meta name="viewport" content="width=device-width, initial-scale=1">
 
 
 <!-- **************** GET HEADER********************* -->
