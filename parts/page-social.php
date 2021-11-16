bb97deedb691d5d5a89ea622e6436899c3302d34
John Abbott College Graphic & Web Design Dept <gwd@johnabbott.qc.ca>
2021-11-16 07:56:17 -0500

    Added social media classes

--- parts/page-social.php
+++ parts/page-social.php
@@ -20,26 +20,26 @@ $current_page_path = get_act_url() . $_SERVER['REQUEST_URI'];
 <ul id="share-buttons">
 
 <!-- Facebook -->
-<li><a href="http://www.facebook.com/sharer.php?u=<?php echo $current_page_path; ?>" target="_blank">Share on Facebook</a></li>
+<li class="facebook"><a href="http://www.facebook.com/sharer.php?u=<?php echo $current_page_path; ?>" target="_blank">Share on Facebook</a></li>
 
 <!-- Twitter -->
-<li><a href="http://twitter.com/share?url=<?php echo $current_page_path; ?>&text=<?php echo the_title(); ?>&hashtags=GWDuates2021" target="_blank">Share on Twitter</a></li>
+<li class="twitter"><a href="http://twitter.com/share?url=<?php echo $current_page_path; ?>&text=<?php echo the_title(); ?>&hashtags=GWDuates2021" target="_blank">Share on Twitter</a></li>
 
 
 <!-- Digg -->
-<li><a href="http://www.digg.com/submit?url=<?php echo $current_page_path; ?>" target="_blank">Share on Digg</a></li>
+<li class="digg"><a href="http://www.digg.com/submit?url=<?php echo $current_page_path; ?>" target="_blank">Share on Digg</a></li>
 
 <!-- Reddit -->
-<li><a href="http://reddit.com/submit?url=<?php echo $current_page_path; ?>&title=<?php echo the_title(); ?>" target="_blank">Share on Reddit</a></li>
+<li class="reddit"><a href="http://reddit.com/submit?url=<?php echo $current_page_path; ?>&title=<?php echo the_title(); ?>" target="_blank">Share on Reddit</a></li>
 
 <!-- LinkedIn -->
-<li><a href="http://www.linkedin.com/shareArticle?mini=true&url=<?php echo $current_page_path; ?>" target="_blank">Share on LinkedIn</a></li>
+<li class="linkedin"><a href="http://www.linkedin.com/shareArticle?mini=true&url=<?php echo $current_page_path; ?>" target="_blank">Share on LinkedIn</a></li>
 
 <!-- Pinterest -->
-<li><a href="javascript:void((function()%7Bvar%20e=document.createElement('script');e.setAttribute('type','text/javascript');e.setAttribute('charset','UTF-8');e.setAttribute('src','http://assets.pinterest.com/js/pinmarklet.js?r='+Math.random()*99999999);document.body.appendChild(e)%7D)());">Share on Pinterest</a></li>
+<li class="pinterest"><a href="javascript:void((function()%7Bvar%20e=document.createElement('script');e.setAttribute('type','text/javascript');e.setAttribute('charset','UTF-8');e.setAttribute('src','http://assets.pinterest.com/js/pinmarklet.js?r='+Math.random()*99999999);document.body.appendChild(e)%7D)());">Share on Pinterest</a></li>
 
 
 <!-- Email -->
-<li><a href="mailto:?Subject=<?php echo the_title(); ?>&Body=I%20saw%20this%20and%20thought%20of%20you!%20 <?php echo $current_page_path; ?>">Share via Email</a></li>
+<li class="email"><a href="mailto:?Subject=<?php echo the_title(); ?>&Body=I%20saw%20this%20and%20thought%20of%20you!%20 <?php echo $current_page_path; ?>">Share via Email</a></li>
 
 </ul>
