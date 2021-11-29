2a85174d6f8376df24cb436c2264d0901509c635
John Abbott College Graphic & Web Design Dept <gwd@johnabbott.qc.ca>
2021-11-29 18:07:05 -0500

    Formatting code

--- header.php
+++ header.php
@@ -24,63 +24,63 @@ if (!defined('ABSPATH'))
 <body <?php body_class(); ?>>
 
 <div class="main-container">
-<!---  CONTAINER DIV OPENS HERE ----->
+<!---  CONTAINER DIV OPENS HERE / CLOSES IN FOOTER.PHP----->
   
 <!--- HEADER STARTS HERE ----->
 <header>
-<div class="header-nav-search">
-<?php get_search_form(); ?>
-
-<?php // ADD NAVIGATION ?>
-
-
-<nav class="primary-navigation">
-<button class="hide-text">Menu</button>
-
-<?php
-  wp_nav_menu(array(
-	'menu_class'	  	=> 'primary-menu',
-	'theme_location' 	=> 'header-menu',
-	'menu_id'	    		=> 'menu',
-  'container'       => 'div',
-	'container_id'		=> 'menu-container',
-));
-?>
-</nav>
-</div>
+    <div class="header-nav-search">
+        
+        <?php get_search_form(); ?>
 
 
-<div id="branding">
-    <?php 
-       if ( function_exists( 'the_custom_logo' ) ) {
-        the_custom_logo();
-    }
-    ?>
+        <?php // ADD NAVIGATION ?>
+        <nav class="primary-navigation">
+        <button class="hide-text">Menu</button>
 
-    
-<div class="brand-texts">
-    <div class="site-title">
-        <a href="<?php echo get_option('home'); ?>">
-          <?php bloginfo('name'); ?>
-        </a>
+        <?php
+          wp_nav_menu(array(
+          'menu_class'	  	=> 'primary-menu',
+          'theme_location' 	=> 'header-menu',
+          'menu_id'	    		=> 'menu',
+          'container'       => 'div',
+          'container_id'		=> 'menu-container',
+        ));
+        ?>
+        </nav>
     </div>
 
-    <div class="description">
-        <a href="<?php echo get_option('home'); ?>">
-          <?php bloginfo('description'); ?>
-          <?php // This is the WP site tagline, not the meta description tag ?>
-        </a>
+
+    <div id="branding">
+        <?php 
+          if ( function_exists( 'the_custom_logo' ) ) {
+            the_custom_logo();
+        }
+        ?>
+
+        
+        <div class="brand-texts">
+
+                <div class="site-title">
+                    <a href="<?php echo get_option('home'); ?>">
+                      <?php bloginfo('name'); ?>
+                    </a>
+                </div>
+
+
+                <div class="description">
+                    <a href="<?php echo get_option('home'); ?>">
+                      <?php bloginfo('description'); ?>
+                      <?php // This is the WP site tagline, not the meta description tag ?>
+                    </a>
+                </div>
+                <!-- END DESCRIPTION -->
+
+          </div>
+
+
+      <!-- END BRANDING -->
     </div>
-    <!-- END DESCRIPTION -->
 
-  </div>
-  <!-- END BRANDING -->
-  </div>
-
-
-
-
-  
 
 </header> 
 <!--- HEADER ENDS HERE -----> 
