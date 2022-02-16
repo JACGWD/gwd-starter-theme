6464e0883e0dab523878cdf6fd43f310398cda53
John Abbott College Graphic & Web Design Dept <gwd@johnabbott.qc.ca>
2022-02-16 08:33:47 -0500

    Added theme support for the blogs page

---
+++ front-page.php
@@ -0,0 +1,28 @@
+<?php
+/* Template Name: Home/Front Page Template */ 
+/* The template for displaying the first page of the site. */
+/* @link https://developer.wordpress.org/themes/basics/template-hierarchy/  */
+
+
+// Add this to all your php files for added security
+
+if (!defined('ABSPATH'))
+	exit; // Exit if accessed directly.
+?>
+
+<?php get_header(); ?>
+<main>
+		
+    <?php
+    if ( have_posts() ) 
+        while ( have_posts() ) : the_post();			
+            get_template_part( 'parts/page', 'content' );			
+        endwhile;			
+    // endif;	
+
+    get_sidebar();
+    ?>
+
+</main>
+<?php get_footer(); ?>
+
--- home.php
+++ home.php
@@ -1,6 +1,6 @@
 <?php
-/* Template Name: Home Page Template */ 
-/* The template for displaying the first page of the site. */
+/* Template Name: Blogs Page Template */ 
+/* The template for displaying the blog page of the site. */
 /* @link https://developer.wordpress.org/themes/basics/template-hierarchy/  */
 
 
@@ -10,19 +10,91 @@ if (!defined('ABSPATH'))
 	exit; // Exit if accessed directly.
 ?>
 
-<?php get_header(); ?>
-<main>
-		
-    <?php
-    if ( have_posts() ) 
-        while ( have_posts() ) : the_post();			
-            get_template_part( 'parts/page', 'content' );			
-        endwhile;			
-    // endif;	
+<?php
+$blog_posts = new WP_Query( 
+    array( 
+        'post_type' => 'post', 
+        'post_status’' => 'publish', 
+        'posts_per_page' => -1 ) 
+    );
+?>
 
-    get_sidebar();
-    ?>
+<?php get_header(); ?>
+
+<main class="blog-posts-page">
+
+<h1>Blog Posts</h1>
+
+<?php if ( $blog_posts->have_posts() ) : ?>
+<? // IF THERE ARE ANY POST START THE LOOP, OTHERWISE GO TO LINE 83 ?>
+
+
+
+        <?php while ( $blog_posts->have_posts() ) : $blog_posts->the_post(); ?>
+        <?php // AS LONG AS THERE ARE POSTS, REPEAT THE FOLLOWING PATTERN ?>
+
+
+    <section class = "blog-post">
+        <article id = "post-<?php the_ID(); ?>">
+
+                <h2 class = "post-title">
+                <?php // CLICKABLE H2 TAG ?>
+                    <a href = "<?php the_permalink(); ?>">
+                        <?php the_title(); ?>
+                    </a>
+                </h2>
+
+
+
+                <ul class="blog-post-metadata">
+                    <li class="post-category"><?php the_category(', '); ?></li>
+                    <?php // ADD BLOG POST CATEGORY ?>
+                    <li class="post-author">by <?php the_author(); ?></li>
+                    <?php // ADD AUTHOR NAME ?>
+                    <li class="post-date">Published on <?php the_date(); ?></li>
+                    <?php // ADD PUBLISHED DATE ?>
+                </ul>
+                
+
+
+                <?php if ( has_post_thumbnail() ) { 
+                 echo '<a href = "<?php the_permalink(); ?>">' .
+                the_post_thumbnail( get_the_ID(), 'full' ) .
+                '</a>';
+                } ?>
+                <?php // ADD CLICKABLE THUMBNAIL IMAGE IF ONE EXISTS ?>
+
+               
+
+                <section class="post-excerpt">
+                    <?php wp_kses_post( the_excerpt() ) ?>
+                    <?php // ADD TEXT EXCERPT OF BLOG POST ?>
+                </section>
+
+
+                <div class = "post-read-more">
+                    <a itemprop = "url" href = "<?php the_permalink(); ?>" target = "_blank">
+                    <?php echo esc_html__( 'Read more', 'gwdstarter' ) ?>
+                    <?php // ADD READ MORE LINK ?>
+                    </a>
+                </div> 
+
+        </article>
+    </section> 
+    <?php endwhile; ?>
+
+
+
+<? // IF NO BLOG POSTS - DO THE FOLLOWING ?>
+<?php else: ?>
+<p class = "no-blog-posts">
+<?php esc_html_e('Sorry, no posts matched your criteria.', 'theme-domain'); ?> 
+</p>
+<?php endif; 
+wp_reset_postdata(); ?>
+</div>
 
 </main>
-<?php get_footer(); ?>
 
+<?php get_sidebar(); ?>
+<?php get_footer(); ?>
